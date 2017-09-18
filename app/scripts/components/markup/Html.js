e1a7e587650cc3ba11f5f4842fd8211c39b31c6f
Julian Tellez <juliantellezmendez@gmail.com>
2017-09-18 21:52:38 +0100

    [ HTML ]: adding google analytics

--- app/scripts/components/markup/Html.js
+++ app/scripts/components/markup/Html.js
@@ -6,6 +6,21 @@ export default class Html extends React.Component {
     title: React.PropTypes.string,
     store: React.PropTypes.object,
   }
+
+  getGoogleAnalytics () {
+    const script = (`
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+      ga('create', 'UA-72315706-2', 'auto');
+      ga('send', 'pageview');
+    `)
+
+    return <script dangerouslySetInnerHTML={{__html: script}} />
+  }
+
   render () {
     const state = JSON.stringify(this.props.store.getState())
     return (
@@ -20,6 +35,7 @@ export default class Html extends React.Component {
           <div id='main' dangerouslySetInnerHTML={{__html: this.props.children}} />
           <script src='/aliway/bundles/client.js'></script>
           <script dangerouslySetInnerHTML={{__html: `window.STATE = ${state}`}}/>
+          {this.getGoogleAnalytics()}
         </body>
       </html>
     )
